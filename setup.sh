696d7ec55bcfc84d2664d18c42286ba8deeae0f3
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-28 11:12:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # https://3dclub.orbbec3d.com/t/orbbec-openi-problem-with-usb/279/5
 # https://github.com/pierrep/ofxOrbbecAstra/tree/master
 
-sudo cp install/orbbec-usb.rules /etc/udev/rules.d/558-orbbec-usb.rules
+sudo cp orbbec-usb.rules /etc/udev/rules.d/558-orbbec-usb.rules
 sudo udevadm control --reload-rules
 
 
