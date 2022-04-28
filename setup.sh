afe7856da329e569dff2dc28a9c26bb6a0216f7d
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-28 11:03:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 # This enables accessing the camera at runtime without sudo
 # https://3dclub.orbbec3d.com/t/orbbec-openi-problem-with-usb/279/5
+# https://github.com/pierrep/ofxOrbbecAstra/tree/master
 
-sudo cp orbbec-usb.rules /etc/udev/rules.d/
+sudo cp install/orbbec-usb.rules /etc/udev/rules.d/558-orbbec-usb.rules
 sudo udevadm control --reload-rules
 
 
