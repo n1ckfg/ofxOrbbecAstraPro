452049de64c19fdc6812fbd1eaa1ce24fa92471f
Nick Fox-Gieg <nick@fox-gieg.com>
2022-05-01 12:58:34 -0400

    example

--- example_color-depth/src/ofApp.cpp
+++ example_color-depth/src/ofApp.cpp
@@ -6,6 +6,15 @@ void ofApp::setup()
 	astraCam.setup();
 	astraCam.setRegistration(true);
 	astraCam.initSelectedStreams(true, true, false);
+	
+	/*
+	// https://www.dropbox.com/sh/bgua92ffs7qdr7r/AACpyRqgeP1U7mwvNqzJPB8ra?dl=0&preview=Orbbec+OpenNI+Extended+API+Instruction.pdf
+	// http://docs.ros.org/en/kinetic/api/astra_camera/html/namespaceopenni.html
+	int dataSize = 4;
+	int ldp_en = 0;
+	int propertyId = 100; //openni::OBEXTENSION_ID_LDP_EN 
+	astraCam.setProperty(propertyId, (uint8_t *) &ldp_en, dataSize);
+	*/
 }
 //------------------------------------------------------------------------
 void ofApp::update()
